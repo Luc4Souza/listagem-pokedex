bc4e2b331e184deb982e2993494c9124217b2761
Lucas  Souza <souza.lucasp10@gmail.com>
2023-05-13 21:48:00 -0300

    js

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+//alert("olá");
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagemBotaotrocaDeTema = document.querySelector(".imagem-botao")
+
+botaoAlterarTema.addEventListener("click", () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    if (modoEscuroEstaAtivo) {
+        body.classList.remove("modo-escuro");
+        imagemBotaotrocaDeTema.setAttribute("src", "./src/imagens/sun.png")
+    } else {
+        body.classList.add("modo-escuro");
+        imagemBotaotrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
+    }
+})
