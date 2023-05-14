12cef514bf58864f90723b2ebea75019c49d5327
Lucas  Souza <souza.lucasp10@gmail.com>
2023-05-13 21:53:29 -0300

    js atulizado

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,11 @@ const imagemBotaotrocaDeTema = document.querySelector(".imagem-botao")
 
 botaoAlterarTema.addEventListener("click", () => {
     const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+    
+    body.classList.toggle("modo-escuro");
     if (modoEscuroEstaAtivo) {
-        body.classList.remove("modo-escuro");
         imagemBotaotrocaDeTema.setAttribute("src", "./src/imagens/sun.png")
     } else {
-        body.classList.add("modo-escuro");
         imagemBotaotrocaDeTema.setAttribute("src", "./src/imagens/moon.png")
     }
 })
